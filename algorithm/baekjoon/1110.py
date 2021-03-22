23ab57b967ec446523e058557b9f12e8317788ce
Adam <kdh92417@gmail.com>
2021-03-22 23:50:32 +0900

    Solve Baekjoon 1110

---
+++ algorithm/baekjoon/1110.py
@@ -0,0 +1,25 @@
+# 백준 1110번
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+origin = n
+count = 0
+
+while True:
+    if (n < 0) or (n > 99):
+        print(0)
+        break
+
+    first_num = n // 10
+    second_num = n % 10
+    merge_num = (first_num + second_num) % 10
+
+    count += 1
+    n = (second_num * 10) + merge_num
+
+    if n == origin:
+        print(count)
+        break
